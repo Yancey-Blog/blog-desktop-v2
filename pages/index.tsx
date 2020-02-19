11fcfd9905c6bf943f6f362373717085ad73edb2
Yancey Leo <yanceyofficial@gmail.com>
2020-02-19 10:37:29 +0800

    feat: add pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <h1>Hello world! - user agent: {userAgent}</h1>
+  <>
+    <h1>Hello world! - user agent: {userAgent}</h1>
+    <Link href="/music">to music page</Link>
+  </>
 )
 
 Home.getInitialProps = async ({ req }) => {
