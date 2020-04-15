7f8e19a58845dc4d4ffcc4fb919ce6d5ceaca9b1
YanceyOfficial <yanceyofficial@gmail.com>
2020-04-15 17:35:47 +0800

    feat: update config of apollo client

--- pages/music.tsx
+++ pages/music.tsx
@@ -1,28 +1,11 @@
 import React from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
-import Layout from '../src/components/Layout/Layout'
-import Announcement from '../src/components/Home/Announcement'
+import Layout from 'src/components/Layout/Layout'
+import MusicContainer from 'src/containers/Music/MusicContainer'
 
 const Music = () => {
-  const ANNOUNCEMENTS = gql`
-    query GetAnnouncements {
-      getAnnouncements {
-        _id
-        content
-        createdAt
-        updatedAt
-      }
-    }
-  `
-
-  const { data } = useQuery(ANNOUNCEMENTS, {
-    notifyOnNetworkStatusChange: true,
-  })
-
   return (
     <Layout>
-      <Announcement data={data?.getAnnouncements || []} />
+      <MusicContainer />
     </Layout>
   )
 }
--- src/shared/withApollo.ts
+++ src/shared/withApollo.ts
@@ -2,7 +2,6 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BatchHttpLink } from 'apollo-link-batch-http'
 import { onError } from 'apollo-link-error'
-import { persistCache } from 'apollo-cache-persist'
 import withApollo from 'next-with-apollo'
 import fetch from 'isomorphic-unfetch'
 import SnackbarUtils from 'src/components/Toast/Toast'
@@ -24,23 +23,10 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   }
 })
 
-const cache = new InMemoryCache()
-
-async function handlePersistCache() {
-  await persistCache({
-    cache,
-    // @ts-ignore
-    storage: window.localStorage,
-    maxSize: false,
-  })
-}
-
-handlePersistCache()
-
 export default withApollo(
   () =>
     new ApolloClient({
-      cache,
+      cache: new InMemoryCache(),
       resolvers: {},
       link: errorLink.concat(httpLink),
       defaultOptions: {
