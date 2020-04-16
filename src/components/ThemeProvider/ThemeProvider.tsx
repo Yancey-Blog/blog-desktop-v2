c2157c66c41450fd1510718eb4c3008820e54d01
Yancey Leo <yanceyofficial@gmail.com>
2020-04-17 04:15:25 +0800

    feat: add ThemeMode enum for useDarkMode

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -2,11 +2,11 @@ import React, { FC } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme, darkTheme } from 'src/styled/theme'
 import GlobalStyle from 'src/styled/globalStyles'
-import { useDarkMode } from 'src/hooks/useDarkMode'
+import { useDarkMode, ThemeMode } from 'src/hooks/useDarkMode'
 
 const ThemeProviderComponent: FC = ({ children }) => {
   const [theme] = useDarkMode()
-  const themeMode = theme === 'light' ? lightTheme : darkTheme
+  const themeMode = theme === ThemeMode.LIGHT ? darkTheme : lightTheme
 
   return (
     <ThemeProvider theme={themeMode}>
--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,19 +1,24 @@
 import { useEffect, useState } from 'react'
 
+export enum ThemeMode {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
 export const useDarkMode = () => {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState(ThemeMode.LIGHT)
   const toggleTheme = () => {
-    if (theme === 'light') {
-      window.localStorage.setItem('theme', 'dark')
-      setTheme('dark')
+    if (theme === ThemeMode.LIGHT) {
+      window.localStorage.setItem('theme', ThemeMode.DARK)
+      setTheme(ThemeMode.DARK)
     } else {
-      window.localStorage.setItem('theme', 'light')
-      setTheme('light')
+      window.localStorage.setItem('theme', ThemeMode.LIGHT)
+      setTheme(ThemeMode.LIGHT)
     }
   }
 
   useEffect(() => {
-    const localTheme = window.localStorage.getItem('theme')
+    const localTheme = window.localStorage.getItem('theme') as ThemeMode | null
     if (localTheme) {
       setTheme(localTheme)
     }
