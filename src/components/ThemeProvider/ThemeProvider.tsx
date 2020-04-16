cff3707a584fc23e06b3f3ae8ad04ecc0c4b016b
Yancey Leo <yanceyofficial@gmail.com>
2020-04-17 04:06:39 +0800

    feat: migrate styled-component to styled file folder

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import { ThemeProvider } from 'styled-components'
-import { lightTheme, darkTheme } from 'src/shared/theme'
-import GlobalStyle from 'src/shared/globalStyles'
+import { lightTheme, darkTheme } from 'src/styled/theme'
+import GlobalStyle from 'src/styled/globalStyles'
 import { useDarkMode } from 'src/hooks/useDarkMode'
 
 const ThemeProviderComponent: FC = ({ children }) => {
