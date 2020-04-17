e27cd5f57fd027324031da91913f9f0d8d421c6d
Yancey Leo <yanceyofficial@gmail.com>
2020-04-17 18:57:10 +0800

    feat: finish Motto

--- src/containers/Home/components/Motto.tsx
+++ src/containers/Home/components/Motto.tsx
@@ -1,53 +1,9 @@
 import React, { FC } from 'react'
 import styled, { css } from 'styled-components'
-import { SOCIAL_MEDIA } from 'src/shared/constants'
+import { SOCIAL_MEDIA, ALI_OSS_URL } from 'src/shared/constants'
 import { flexMixin } from 'src/styled/mixins'
 import { IMotto } from '../types'
 import svgIcons from '../../../../static/yancey-official-blog-svg-icons.svg'
-
-export const QRCode = css`
-  &::after,
-  &::before {
-    position: absolute;
-    content: '';
-    opacity: 0;
-    visibility: hidden;
-    transform: translate3d(0, 16px, 0);
-    transition: 700ms all ease;
-  }
-
-  &::after {
-    width: 120px;
-    height: 120px;
-    padding: 10px;
-    border-radius: 12px;
-    background-size: cover;
-    background-origin: content-box;
-    top: 62px;
-    left: -58px;
-  }
-
-  &::before {
-    width: 0;
-    height: 0;
-    border: 16px solid transparent;
-    border-bottom-color: rgba(0, 0, 0, 0.5);
-    top: 30px;
-    left: -4px;
-
-    background: rgba(0, 0, 0, 0.5)
-      url(https://static.yancey.app/blog-fe-static/official-account-qr-code.jpg)
-      no-repeat center top;
-  }
-
-  &:hover::after,
-  &:hover::before {
-    opacity: 1;
-    visibility: visible;
-    transform: translate3d(0, 0, 0);
-    transition: 700ms all ease;
-  }
-`
 
 export const MottoBar = styled.section`
   position: relative;
@@ -88,7 +44,7 @@ export const Svg = styled.svg`
   cursor: pointer;
 `
 
-export const Link = styled.a`
+export const QRCode = css`
   position: relative;
 
   &::after,
@@ -102,36 +58,51 @@ export const Link = styled.a`
   }
 
   &::after {
-    width: 120px;
-    height: 120px;
-    padding: 10px;
-    border-radius: 12px;
-    top: 62px;
-    left: -44px;
-    background: rgba(0, 0, 0, 0.5)
-      url(https://static.yancey.app/blog-fe-static/official-account-qr-code.jpg)
-      no-repeat center top;
-    background-size: cover;
-    background-origin: content-box;
+    width: 8rem;
+    height: 8rem;
+    padding: 0.6666666666666666rem;
+    border-radius: 0.8rem;
+    top: 4.133333333333334rem;
+    left: -3.8666666666666667rem;
   }
 
   &::before {
     width: 0;
     height: 0;
-    border: 16px solid transparent;
+    border: 1.0666666666666667rem solid transparent;
     border-bottom-color: rgba(0, 0, 0, 0.5);
-    top: 30px;
-    left: 10px;
+    top: 2rem;
+    left: -0.26666666666666666rem;
   }
 
-  &:hover {
-    &::after,
-    &::before {
-      opacity: 1;
-      visibility: visible;
-      transform: translate3d(0, 0, 0);
-      transition: 700ms all ease;
-    }
+  &:hover::after,
+  &:hover::before {
+    opacity: 1;
+    visibility: visible;
+    transform: translate3d(0, 0, 0);
+    transition: 700ms all ease;
+  }
+`
+
+export const TwitterQRCodeLink = styled.a`
+  ${QRCode}
+  &::after {
+    background: rgba(0, 0, 0, 0.5)
+      url(${ALI_OSS_URL}/blog-fe-static/twitter-qr-code.jpg) no-repeat center
+      top;
+    background-size: cover;
+    background-origin: content-box;
+  }
+`
+
+export const WeChatQRCodeLink = styled.a`
+  ${QRCode}
+  &::after {
+    background: rgba(0, 0, 0, 0.5)
+      url(${ALI_OSS_URL}/blog-fe-static/official-account-qr-code.jpg) no-repeat
+      center top;
+    background-size: cover;
+    background-origin: content-box;
   }
 `
 
@@ -146,18 +117,50 @@ const Motto: FC<Props> = ({ mottos }) => {
       <MottoContent>{mottos[0]?.content}</MottoContent>
 
       <SocialMediaIconWrapper>
-        {Object.keys(SOCIAL_MEDIA).map((key) => (
-          <Link
-            key={key}
-            href={SOCIAL_MEDIA[key].url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Svg>
-              <use xlinkHref={`${svgIcons}${SOCIAL_MEDIA[key].icon}`} />
-            </Svg>
-          </Link>
-        ))}
+        {Object.keys(SOCIAL_MEDIA).map((key) => {
+          if (key === 'twitter') {
+            return (
+              <TwitterQRCodeLink
+                key={key}
+                href={SOCIAL_MEDIA[key].url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Svg>
+                  <use xlinkHref={`${svgIcons}${SOCIAL_MEDIA[key].icon}`} />
+                </Svg>
+              </TwitterQRCodeLink>
+            )
+          }
+
+          if (key === 'wechat') {
+            return (
+              <WeChatQRCodeLink
+                key={key}
+                href={SOCIAL_MEDIA[key].url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Svg>
+                  <use xlinkHref={`${svgIcons}${SOCIAL_MEDIA[key].icon}`} />
+                </Svg>
+              </WeChatQRCodeLink>
+            )
+          }
+
+          return (
+            <a
+              key={key}
+              href={SOCIAL_MEDIA[key].url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Svg>
+                <use xlinkHref={`${svgIcons}${SOCIAL_MEDIA[key].icon}`} />
+              </Svg>
+            </a>
+          )
+        })}
       </SocialMediaIconWrapper>
     </MottoBar>
   )
