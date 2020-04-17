341fb5c894185c6873284d71e190a2f3e84dd644
Yancey Leo <yanceyofficial@gmail.com>
2020-04-17 15:53:28 +0800

    feat: update Slogan

--- src/containers/Home/components/Announcement.tsx
+++ src/containers/Home/components/Announcement.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
 import { IAnnouncement } from '../types'
 
 interface Props {
   announcements: IAnnouncement[]
 }
+
+export const AnnouncementBar = styled.section`
+  position: relative;
+`
 
 const Announcement: FC<Props> = ({ announcements }) => {
   return <div>{announcements[0]?.content}</div>
--- src/containers/Home/components/Slogan.tsx
+++ src/containers/Home/components/Slogan.tsx
@@ -3,49 +3,49 @@ import styled from 'styled-components'
 import { noUserSelectMixin } from 'src/styled/mixins'
 import { glitchLoop1, glitchLoop2 } from 'src/styled/animations'
 
-const Slogan: FC = () => {
-  const Slogans = styled.h2`
+const Slogans = styled.h2`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin: 0 auto 1.5rem;
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 5rem;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.white};
+  transform: translate3d(-50%, -40%, 0);
+  ${noUserSelectMixin()}
+
+  &::before,
+&::after {
     position: absolute;
-    left: 50%;
-    top: 50%;
-    margin: 0 auto 1.5rem;
-    font-family: 'Ubuntu', sans-serif;
-    font-size: 5rem;
-    text-align: center;
-    color: ${({ theme }) => theme.colors.white};
-    transform: translate3d(-50%, -40%, 0);
-    ${noUserSelectMixin()}
+    content: attr(data-slogan);
+    top: 0;
+    width: 100%;
+    clip: rect(0, 0, 0, 0);
+  }
 
-    &::before,
-    &::after {
-      position: absolute;
-      content: attr(data-slogan);
-      top: 0;
-      width: 100%;
-      clip: rect(0, 0, 0, 0);
-    }
+  &::before {
+    left: -1px;
+    text-shadow: ${({ theme }) => `1px 0 ${theme.colors.sloganRed}`};
+  }
 
-    &::before {
-      left: -1px;
-      text-shadow: ${({ theme }) => `1px 0 ${theme.colors.sloganRed}`};
-    }
+  &::after {
+    left: 1px;
+    text-shadow: ${({ theme }) => `-1px 0 ${theme.colors.sloganBlue}`};
+  }
 
-    &::after {
-      left: 1px;
-      text-shadow: ${({ theme }) => `-1px 0 ${theme.colors.sloganBlue}`};
-    }
+  &:hover::before {
+    text-shadow: ${({ theme }) => `4px 0 ${theme.colors.sloganRed}`};
+    animation: ${glitchLoop1} 0.8s infinite ease-in-out alternate-reverse;
+  }
 
-    &:hover::before {
-      text-shadow: ${({ theme }) => `4px 0 ${theme.colors.sloganRed}`};
-      animation: ${glitchLoop1} 0.8s infinite ease-in-out alternate-reverse;
-    }
+  &:hover::after {
+    text-shadow: ${({ theme }) => `-5px 0 ${theme.colors.sloganBlue}`};
+    animation: ${glitchLoop2} 0.8s infinite ease-in-out alternate-reverse;
+  }
+`
 
-    &:hover::after {
-      text-shadow: ${({ theme }) => `-5px 0 ${theme.colors.sloganBlue}`};
-      animation: ${glitchLoop2} 0.8s infinite ease-in-out alternate-reverse;
-    }
-  `
-
+const Slogan: FC = () => {
   return <Slogans data-slogan="HI, YANCEY!">HI, YANCEY!</Slogans>
 }
 
