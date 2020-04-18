1c32c80345e15408b9e5abee162df05eeb3cb4da
Yancey Leo <yanceyofficial@gmail.com>
2020-04-19 05:48:30 +0800

    feat: update preview

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req)
   res.setPreviewData({})
   res.end('Preview mode enabled')
 }
