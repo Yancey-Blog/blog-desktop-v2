e4ecd6d41bcd5fa010805cbb57b2a062ff30b083
Yancey Leo <yanceyofficial@gmail.com>
2020-04-20 23:26:50 +0800

    feat: iniitial archive page

---
+++ src/containers/Archive/Archive.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import { ArchiveWrapper } from './styled'
+
+const Archive: FC = () => {
+  return <ArchiveWrapper>h</ArchiveWrapper>
+}
+
+export default Archive
---
+++ src/containers/Music/Music.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Music = () => {
+  return <div>Music</div>
+}
+
+export default Music
