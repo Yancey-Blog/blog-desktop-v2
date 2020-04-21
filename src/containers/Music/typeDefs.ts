1c7f5ab0389acd0d986a11600072a00cde882718
Yancey Leo <yanceyofficial@gmail.com>
2020-04-21 18:48:43 +0800

    feat: add types and typeDefs for Music component

--- src/containers/Music/typeDefs.ts
+++ src/containers/Music/typeDefs.ts
@@ -11,3 +11,43 @@ export const PLAYERS = gql`
     }
   }
 `
+
+export const BEST_ALBUMS = gql`
+  query GetBestAlbums {
+    _id
+    title
+    artist
+    coverUrl
+    mvUrl
+    releaseDate
+    createdAt
+    updatedAt
+  }
+`
+
+export const LIVE_TOURS = gql`
+  query GetLiveTours {
+    getLiveTours {
+      _id
+      title
+      posterUrl
+      showTime
+      createdAt
+      updatedAt
+    }
+  }
+`
+
+export const YANCEY_MUSIC = gql`
+  query GetYanceyMusic {
+    getYanceyMusic {
+      _id
+      title
+      soundCloudUrl
+      posterUrl
+      releaseDate
+      createdAt
+      updatedAt
+    }
+  }
+`
--- src/containers/Music/types.ts
+++ src/containers/Music/types.ts
@@ -9,3 +9,45 @@ export interface IPlayer {
 export interface PlayerQuery {
   players: IPlayer[]
 }
+
+export interface IBestAlbum {
+  readonly _id: string
+  readonly title: string
+  readonly artist: string
+  readonly coverUrl: string
+  readonly mvUrl: string
+  readonly releaseDate: string
+  readonly createdAt: string
+  readonly updatedAt: string
+}
+
+export interface BestAlbumQuery {
+  getBestAlbums: IBestAlbum[]
+}
+
+export interface ILiveTour {
+  _id: string
+  title: string
+  showTime: string
+  posterUrl: string
+  createdAt: string
+  updatedAt: string
+}
+
+export interface LiveTourQuery {
+  getLiveTours: ILiveTour[]
+}
+
+export interface IYanceyMusic {
+  readonly _id: string
+  readonly title: string
+  readonly soundCloudUrl: string
+  readonly posterUrl: string
+  readonly releaseDate: string
+  readonly createdAt: string
+  readonly updatedAt: string
+}
+
+export interface YanceyMusicQuery {
+  getYanceyMusic: IYanceyMusic[]
+}
