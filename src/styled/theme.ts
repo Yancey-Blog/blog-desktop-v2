4af437bcd42879ec850bd5611032e2d1fff34193
YanceyOfficial <yanceyofficial@gmail.com>
2020-04-22 13:55:38 +0800

    feat: update BestAlbum styles of box-shadow color

--- src/styled/theme.ts
+++ src/styled/theme.ts
@@ -4,6 +4,7 @@ const light = {
     secondary: '#fbfbfb',
     postCard: '#ffffff',
     blurCard: 'rgba(245, 245, 245, .8)',
+    bestAlbumCard: '#e6e6e6',
   },
   text: {
     primary: '#666666',
@@ -26,6 +27,7 @@ const dark = {
     secondary: '#1d1f27',
     postCard: '#1d1f27',
     blurCard: 'rgba(27, 29, 30, .8)',
+    bestAlbumCard: '#000000',
   },
   text: {
     primary: '#f5f6f7',
