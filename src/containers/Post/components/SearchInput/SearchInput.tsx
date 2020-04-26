06f3dcf3484c4b611288a44d4a75b481d36b33f6
Yancey Leo <yanceyofficial@gmail.com>
2020-04-26 14:37:16 +0800

    feat: finish SearchInputWrapper component

--- src/containers/Post/components/SearchInput/SearchInput.tsx
+++ src/containers/Post/components/SearchInput/SearchInput.tsx
@@ -1,5 +1,33 @@
 import React, { FC, useState, ChangeEvent, KeyboardEvent } from 'react'
+import styled from 'styled-components'
 import { useRouter } from 'next/router'
+import { SVG_SPRITE } from 'src/shared/constants'
+import svgIcons from 'src/static/svg-sprite.svg'
+
+const SearchInputWrapper = styled.div`
+  position: relative;
+`
+
+const Input = styled.input`
+  margin-left: 1.6rem;
+  padding: 0.8rem;
+  width: 13rem;
+  font-size: 0.9rem;
+  color: ${({ theme }) => theme.text.header};
+  background: ${({ theme }) => theme.background.bestAlbumCard};
+  border: none;
+  border-radius: 0.3rem;
+  outline: none;
+`
+
+const SearchSvg = styled.svg`
+  position: absolute;
+  margin: 0 !important;
+  top: 0.73rem;
+  right: 0.73rem;
+  width: 1.2rem !important;
+  height: 1.2rem !important;
+`
 
 const SearchInput: FC = () => {
   const router = useRouter()
@@ -20,11 +48,16 @@ const SearchInput: FC = () => {
   }
 
   return (
-    <input
-      type="text"
-      onChange={handleInputChange}
-      onKeyUp={(e) => onSubmit(e)}
-    />
+    <SearchInputWrapper>
+      <Input
+        type="text"
+        onChange={handleInputChange}
+        onKeyUp={(e) => onSubmit(e)}
+      />
+      <SearchSvg>
+        <use xlinkHref={`${svgIcons}${SVG_SPRITE.search1}`} />
+      </SearchSvg>
+    </SearchInputWrapper>
   )
 }
 
