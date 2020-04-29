ca3f2321f3ea5cd7f60a676e45f50fc0721b813e
YanceyOfficial <yanceyofficial@gmail.com>
2020-04-29 19:26:14 +0800

    feat: add 404 and 5xx basic page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import Layout from 'src/containers/Layout/Layout'
+
+const NotFound: FC = () => {
+  return <Layout>404</Layout>
+}
+
+export default NotFound
---
+++ src/pages/_error.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import Layout from 'src/containers/Layout/Layout'
+
+const ServerError: FC = () => {
+  return <Layout>500</Layout>
+}
+
+export default ServerError
