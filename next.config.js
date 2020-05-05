f4f9fe20d75036603ea1a4e759760900d626d4dc
Yancey Leo <yanceyofficial@gmail.com>
2020-05-05 23:41:16 +0800

    chore: integrate @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,29 +1,34 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 const withSourceMaps = require('@zeit/next-source-maps')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 module.exports = (phase, { defaultConfig }) =>
-  withSourceMaps({
-    env: {
-      BASE_URL:
-        phase === PHASE_DEVELOPMENT_SERVER
-          ? 'http://localhost:3002/graphql'
-          : 'https://api.yanceyleo.com/graphql',
-    },
-    compress: true,
-    pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
-    webpack: (config, options) => {
-      config.module.rules.push({
-        test: [/\.svg/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.webp$/],
-        use: [
-          {
-            loader: 'url-loader',
-          },
-        ],
-      })
+  withBundleAnalyzer(
+    withSourceMaps({
+      env: {
+        BASE_URL:
+          phase === PHASE_DEVELOPMENT_SERVER
+            ? 'http://localhost:3002/graphql'
+            : 'https://api.yanceyleo.com/graphql',
+      },
+      compress: true,
+      pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
+      webpack: (config, options) => {
+        config.module.rules.push({
+          test: [/\.svg/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.webp$/],
+          use: [
+            {
+              loader: 'url-loader',
+            },
+          ],
+        })
 
-      return config
-    },
-    experimental: {
-      jsconfigPaths: true,
-    },
-  })
+        return config
+      },
+      experimental: {
+        jsconfigPaths: true,
+      },
+    }),
+  )
