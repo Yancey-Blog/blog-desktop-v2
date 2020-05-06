e55c3fe4cf8d3c5308cdfa32e4504371b1c9061d
YanceyOfficial <yanceyofficial@gmail.com>
2020-05-06 20:02:40 +0800

    fix: make dymanic router's prefetch to false

--- src/context/WebpContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-import { checkWebp } from 'yancey-js-util'
-
-const WebpContext = createContext(
-  typeof document !== 'undefined' && checkWebp(),
-)
-
-export default WebpContext
