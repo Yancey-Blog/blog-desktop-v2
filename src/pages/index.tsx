0210e0be4cd2341ffb081f0cd76c031edc371662
Yancey Leo <yanceyofficial@gmail.com>
2020-05-13 01:30:51 +0800

    feat: get headers['accept'] at request stage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import { GetServerSidePropsContext } from 'next'
 import Layout from 'src/containers/Layout/Layout'
 import HomeContainer from 'src/containers/Home/Home'
 
-const Index = () => {
+const Index = ({ isSupportWebp }: { isSupportWebp: boolean }) => {
+  console.log(isSupportWebp)
   return (
     <Layout>
       <HomeContainer />
     </Layout>
   )
+}
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  return {
+    props: {
+      isSupportWebp: ctx.req.headers.accept?.includes('image/webp'),
+    },
+  }
 }
 
 export default Index
