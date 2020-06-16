f0a38fdb84089ee27a78fc21fa35ab2fee39632c
Yancey Leo <yanceyofficial@gmail.com>
2020-06-16 20:13:54 +0800

    feat: support new version of highlight.js

--- src/containers/Post/PostDetail/tools.ts
+++ src/containers/Post/PostDetail/tools.ts
@@ -1,4 +1,7 @@
 import React from 'react'
+// @ts-ignore
+// FIXME: A new version of highlight.js's declaration file
+// has not been released.
 import hljs from 'highlight.js'
 import tocbot from 'tocbot'
 import { DOMAIN } from 'src/shared/constants'
