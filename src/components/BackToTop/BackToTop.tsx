a1618d6973aeea5662724b68203d700cecda318f
Yancey Leo <yanceyofficial@gmail.com>
2020-06-29 17:23:33 +0800

    feat: support throttle for BackToTop component

--- src/components/BackToTop/BackToTop.tsx
+++ src/components/BackToTop/BackToTop.tsx
@@ -1,21 +1,24 @@
 import React, { FC, useState, useEffect } from 'react'
+import throttle from 'lodash.throttle'
 import { scrollToTop } from 'src/shared/utils'
 import { Cat } from './styled'
 
 const BackToTop: FC = () => {
   const [showCat, setShowCat] = useState('')
 
-  const scrollToTopHander = () => {
+  const scrollToTopHander = throttle(() => {
     const top = document.documentElement.scrollTop || document.body.scrollTop
     if (top > 800) {
       setShowCat('showCat')
     } else {
       setShowCat('')
     }
-  }
+  }, 100)
 
   useEffect(() => {
-    document.addEventListener('scroll', scrollToTopHander, { passive: true })
+    document.addEventListener('scroll', scrollToTopHander, {
+      passive: true,
+    })
 
     return () => {
       document.removeEventListener('scroll', scrollToTopHander)
