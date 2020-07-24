a91b29aa71481c391ff114fd05c68646a6a25d43
Yancey Leo <yanceyofficial@gmail.com>
2020-07-24 14:49:43 +0800

    feat: package scrollTop listener to Layout component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
-import React, { FC, useState, useEffect } from 'react'
+import React, { FC } from 'react'
 import Link from 'next/link'
-import throttle from 'lodash.throttle'
 import SearchInput from 'src/containers/Post/components/SearchInput/SearchInput'
 import { SVG_SPRITE } from 'src/shared/constants'
 import { IGlobalSetting } from 'src/containers/GlobalSetting/types'
@@ -8,37 +7,14 @@ import { Logo, NavBar, NavBarItem } from './styled'
 
 interface Props {
   globalSetting: IGlobalSetting
+  isTop: boolean
 }
 
-const Header: FC<Props> = ({ globalSetting }) => {
+const Header: FC<Props> = ({ globalSetting, isTop }) => {
   const { cvPostId } = globalSetting
 
-  const [isTop, setIsTop] = useState(true)
-
-  const isTopHandler = throttle(() => {
-    const top = document.documentElement.scrollTop || document.body.scrollTop
-    if (top === 0) {
-      setIsTop(true)
-    } else {
-      setIsTop(false)
-    }
-  }, 100)
-
-  useEffect(() => {
-    document.addEventListener('scroll', isTopHandler, {
-      passive: true,
-    })
-
-    return () => {
-      document.removeEventListener('scroll', isTopHandler)
-    }
-  }, [])
-
   return (
-    <NavBar
-      // TODO: use className!
-      style={isTop ? { background: 'transparent', boxShadow: 'none' } : {}}
-    >
+    <NavBar className={isTop ? 'hideNavBarBackground' : ''}>
       <Link href="/" passHref>
         <Logo />
       </Link>
