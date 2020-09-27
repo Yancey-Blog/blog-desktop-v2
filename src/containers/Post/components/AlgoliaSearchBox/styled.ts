8baba25070a06832f87dcbe50b16a5ed4a5c1e9e
Yancey Leo <yanceyofficial@gmail.com>
2020-09-27 15:20:31 +0800

    feat: update styles

--- src/containers/Post/components/AlgoliaSearchBox/styled.ts
+++ src/containers/Post/components/AlgoliaSearchBox/styled.ts
@@ -4,7 +4,7 @@ export const SearchBoxContainer = styled.section`
   position: fixed;
   top: 4.6rem;
   right: 0;
-  width: 43.5rem;
+  width: 30rem;
   height: 100%;
   background: #fff;
   box-shadow: 0 2px 64px 0 rgba(0, 0, 0, 0.15);
