a58dc727394b731439fcf691660dae6b7bd9e771
Yancey Leo <yanceyofficial@gmail.com>
2020-09-27 15:18:51 +0800

    feat: update aligolia

--- src/containers/Post/components/AlgoliaSearchBox/AlgoliaSearchBox.tsx
+++ src/containers/Post/components/AlgoliaSearchBox/AlgoliaSearchBox.tsx
@@ -8,6 +8,7 @@ import {
   Highlight,
   Configure,
 } from 'react-instantsearch-dom'
+import { SearchBoxContainer, Result } from './styled'
 
 const ALGOLIA_SEARCH_APP_ID = '5Y6Y04WE04'
 const ALGOLIA_SEARCH_API_KEY = '46f32897c2a83b6495111a68bd1cd8c7'
@@ -24,36 +25,41 @@ const Hit = ({ hit }: any) => (
     <div className="hit-name">
       <Highlight attribute="name" hit={hit} />
     </div>
-    <hr />
+    <br />
     <div className="hit-description">
       <Highlight attribute="description" hit={hit} />
     </div>
-    <hr />
+    <br />
     <div className="hit-content">
       <Snippet hit={hit} attribute="content" />
     </div>
+    <hr />
   </>
 )
 
 const AlgoliaSearchBox: FC = () => {
   return (
-    <div className="ais-InstantSearch">
-      <InstantSearch
-        indexName={ALGOLIA_SEARCH_INDEX_NAME}
-        searchClient={searchClient}
-      >
-        <Configure
-          attributesToSnippet={['content:80']}
-          snippetEllipsisText="..."
-          // highlightPreTag='<em class="search-highlight">'
-          // highlightPostTag="</em>"
-        />
-        <div className="right-panel">
-          <SearchBox />
-          <Hits hitComponent={Hit} />
-        </div>
-      </InstantSearch>
-    </div>
+    <SearchBoxContainer>
+      <div className="ais-InstantSearch">
+        <InstantSearch
+          indexName={ALGOLIA_SEARCH_INDEX_NAME}
+          searchClient={searchClient}
+        >
+          <Configure
+            attributesToSnippet={['content:80']}
+            snippetEllipsisText="..."
+            // highlightPreTag='<em class="search-highlight">'
+            // highlightPostTag="</em>"
+          />
+          <div className="right-panel">
+            <SearchBox />
+            <Result>
+              <Hits hitComponent={Hit} />
+            </Result>
+          </div>
+        </InstantSearch>
+      </div>
+    </SearchBoxContainer>
   )
 }
 
---
+++ src/containers/Post/components/AlgoliaSearchBox/styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const SearchBoxContainer = styled.section`
+  position: fixed;
+  top: 4.6rem;
+  right: 0;
+  width: 43.5rem;
+  height: 100%;
+  background: #fff;
+  box-shadow: 0 2px 64px 0 rgba(0, 0, 0, 0.15);
+  z-index: 2;
+`
+
+export const Result = styled.div`
+  position: absolute;
+  height: 100%;
+  overflow-y: scroll;
+`
