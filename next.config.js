d928f07ee4a0d90a39ac1ad68c6859c213aa43a0
Yancey Leo <yanceyofficial@gmail.com>
2021-06-06 18:56:55 +0800

    feat: update to webpack5

--- next.config.js
+++ next.config.js
@@ -2,13 +2,15 @@ const withSourceMaps = require('@zeit/next-source-maps')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-const webpack = require('webpack')
 
 module.exports = (phase, { defaultConfig }) =>
   withBundleAnalyzer(
     withSourceMaps({
       compress: true,
       pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
+      future: {
+        webpack5: true,
+      },
       webpack: (
         config,
         { buildId, dev, isServer, defaultLoaders, webpack },
