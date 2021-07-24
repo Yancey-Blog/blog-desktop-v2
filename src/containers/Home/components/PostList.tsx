1f5cf3ba06065e21388b9e51c81424ccf0c8c262
YanceyOfficial <yanceyofficial@gmail.com>
2021-07-24 21:11:18 +0800

    feat: supports infinite scroll

--- src/containers/Home/components/PostList.tsx
+++ src/containers/Home/components/PostList.tsx
@@ -1,34 +1,43 @@
-import { FC, useState, useEffect, useRef } from 'react'
+import { FC, useState } from 'react'
 import { useLazyQuery } from '@apollo/client'
 import { SVG_SPRITE } from 'src/shared/constants'
 import PostCard from 'src/containers/Post/components/PostCard/PostCard'
 import { POSTS } from 'src/containers/Post/typeDefs'
 import { PostQuery, PostVars, IPostItem } from 'src/containers/Post/types'
+import InfiniteScroll from 'src/components/InfiniteScroll/InfiniteScroll'
 import SubTitle from './SubTitle'
 
 interface Props {
   isSupportWebp: boolean
 }
 
+interface List {
+  posts: IPostItem[]
+}
+
 const PostList: FC<Props> = ({ isSupportWebp }) => {
-  const [dataSource, setPosts] = useState<IPostItem[]>([])
-  const [pageNum, setPageNum] = useState(1)
+  const [data, setData] = useState<List>({ posts: [] })
+  const [page, setPage] = useState(1)
+  const [hasMore, setHasMore] = useState(true)
 
-  const indicatorRef = useRef<HTMLDivElement>(null)
-
-  const [getPosts] = useLazyQuery<PostQuery, PostVars>(POSTS, {
-    notifyOnNetworkStatusChange: true,
+  const [getPosts, { loading }] = useLazyQuery<PostQuery, PostVars>(POSTS, {
     onCompleted(continuousPosts) {
-      setPageNum(continuousPosts.posts.page + 1)
-      setPosts([...dataSource, ...continuousPosts.posts.items])
+      const { items, total, page: currentPage } = continuousPosts.posts
+      if (Math.ceil(total / 10) === currentPage) {
+        setHasMore(false)
+      }
+      setPage((_page) => _page + 1)
+      setData({ posts: data.posts.concat(items) })
     },
   })
 
   const fetchPosts = (tag?: string) => {
+    if (loading) return
+
     getPosts({
       variables: {
         input: {
-          page: pageNum,
+          page,
           pageSize: 10,
           tag,
         },
@@ -36,33 +45,19 @@ const PostList: FC<Props> = ({ isSupportWebp }) => {
     })
   }
 
-  const callback = (entries: IntersectionObserverEntry[]) => {
-    const entry = entries[0]
-
-    if (entry.isIntersecting) {
-      fetchPosts()
-    }
-  }
-
-  useEffect(() => {
-    fetchPosts()
-
-    if (indicatorRef.current) {
-      const io = new IntersectionObserver(callback)
-      io.observe(indicatorRef.current)
-    }
-  }, [])
-
   return (
-    <>
+    <InfiniteScroll
+      hasMoreData={hasMore}
+      isLoading={loading}
+      onBottomHit={fetchPosts}
+      loadOnMount
+    >
       <SubTitle icon={SVG_SPRITE.new} title="The Latest!" />
 
-      {dataSource.map((post: IPostItem) => (
+      {data.posts.map((post: IPostItem) => (
         <PostCard isSupportWebp={isSupportWebp} post={post} key={post._id} />
       ))}
-
-      <div ref={indicatorRef} />
-    </>
+    </InfiniteScroll>
   )
 }
 
