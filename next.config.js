1a273992c425bb67b1818bd5d32864c84ec6e133
Yancey Leo <yanceyofficial@gmail.com>
2021-08-01 13:22:14 +0800

    feat: supports PWA

--- next.config.js
+++ next.config.js
@@ -2,21 +2,33 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = (phase, { defaultConfig }) =>
-  withBundleAnalyzer({
-    compress: true,
-    pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
-    productionBrowserSourceMaps: true,
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.module.rules.push({
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.webp$/, /\.cur$/],
-        use: [
-          {
-            loader: 'url-loader',
-          },
-        ],
-      })
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-      return config
-    },
-  })
+module.exports = (phase, { defaultConfig }) =>
+  withPWA(
+    withBundleAnalyzer({
+      compress: true,
+      pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
+      productionBrowserSourceMaps: true,
+      pwa: {
+        dest: 'public',
+        runtimeCaching,
+      },
+      webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, webpack },
+      ) => {
+        config.module.rules.push({
+          test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.webp$/, /\.cur$/],
+          use: [
+            {
+              loader: 'url-loader',
+            },
+          ],
+        })
+
+        return config
+      },
+    }),
+  )
