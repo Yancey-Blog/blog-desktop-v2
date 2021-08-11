3f89399652814b63f1f1a961abceb216c042c278
YanceyOfficial <yanceyofficial@gmail.com>
2021-08-11 14:13:16 +0800

    fix: lazy load image's height should be set 16rem only at mobile

--- src/components/LazyLoadImage/LazyLoadImage.tsx
+++ src/components/LazyLoadImage/LazyLoadImage.tsx
@@ -3,6 +3,7 @@ import ProgressiveImage from 'react-progressive-graceful-image'
 import styled from 'styled-components'
 import { generateAliOSSSuffix } from 'src/shared/utils'
 import { AliOSSSuffix } from 'src/shared/constants'
+import breakpoints from 'src/styled/breakpoints'
 
 interface Props {
   isSupportWebp?: boolean
@@ -12,8 +13,12 @@ interface Props {
 
 export const Image = styled.img`
   width: 100%;
-  height: 16rem;
+  height: 100%;
   object-fit: cover;
+
+  @media only screen and ${breakpoints.device.laptop} {
+    height: 16rem;
+  }
 
   &.preview {
     filter: blur(0.4rem);
