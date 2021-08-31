fd71aa873d2da55476a9dc90e8fc86f4bac97c1e
YanceyOfficial <yanceyofficial@gmail.com>
2021-08-31 11:38:41 +0800

    chore: enhances the attached CSS class name on each component

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    [
+      'babel-plugin-styled-components',
+      {
+        ssr: true,
+        pure: true,
+        displayName: process.env.NODE_ENV !== 'production',
+        fileName: false,
+        minify: true,
+        transpileTemplateLiterals: true,
+      },
+    ],
+    [
+      'babel-plugin-transform-imports',
+      {
+        '@material-ui/core': {
+          transform: '@material-ui/core/${member}',
+          preventFullImport: true,
+        },
+        '@material-ui/lab': {
+          transform: '@material-ui/lab/${member}',
+          preventFullImport: true,
+        },
+      },
+    ],
+  ],
+}
